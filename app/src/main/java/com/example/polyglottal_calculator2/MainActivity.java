a11873fe748a641e5eed5e89689149e47483e69f
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-02-21 22:57:34 +0900

    feat: calculate amr

--- app/src/main/java/com/example/polyglottal_calculator2/MainActivity.java
+++ app/src/main/java/com/example/polyglottal_calculator2/MainActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mEditText1;
-    private EditText mEditText2;
+    private EditText mEditTextWeight;
+    private EditText mEditTextHeight;
+    private EditText mEditTextName;
+    private EditText mEditTextAge;
+    private Spinner mSpinnerGender;
+    private Spinner mSpinnerActivity;
     private TextView mTextViewResult;
     private Button mButtonCalc;
 
@@ -19,18 +24,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEditText1 = findViewById(R.id.input_weight);
-        mEditText2 = findViewById(R.id.input_height);
+        mEditTextWeight = findViewById(R.id.input_weight);
+        mEditTextHeight = findViewById(R.id.input_height);
+        mEditTextName = findViewById(R.id.input_name);
+        mEditTextAge = findViewById(R.id.input_age);
+        mSpinnerGender = findViewById(R.id.input_gender);
+        mSpinnerActivity = findViewById(R.id.activity_level);
         mTextViewResult = findViewById(R.id.result);
         mButtonCalc = findViewById(R.id.calc_button);
 
         mButtonCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int num1 = Integer.parseInt(mEditText1.getText().toString());
-                int num2 = Integer.parseInt(mEditText2.getText().toString());
-                int sum = num1 + num2;
-                mTextViewResult.setText(String.valueOf(sum));
+                int weight = Integer.parseInt(mEditTextWeight.getText().toString());
+                int height = Integer.parseInt(mEditTextHeight.getText().toString());
+                int age = Integer.parseInt(mEditTextAge.getText().toString());
+                String name = mEditTextName.getText().toString();
+                Boolean isMale = mSpinnerGender.getSelectedItem().toString().equals("Male");
+                String activity = mSpinnerActivity.getSelectedItem().toString();
+                double bmr;
+                double coefficient;
+
+                if (activity.equals("Sedantary")) {
+                    coefficient = 1.2;
+                } else if (activity.equals("Light activity")) {
+                    coefficient = 1.375;
+                } else if (activity.equals(("Moderate Activity"))) {
+                    coefficient = 1.55;
+                } else if (activity.equals("Very Active")) {
+                    coefficient = 1.9;
+                } else {
+                    coefficient = 1.0;
+                }
+
+                if (isMale) {
+                    bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+                } else {
+                    bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+                }
+
+                double amr = coefficient * bmr;
+                System.out.println(amr);
+                /** mTextViewResult.setText(String.valueOf(amr)); */
+                mTextViewResult.setText(String.format("%4.3f", amr));
+
             }
         });
     }
